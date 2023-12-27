45efbdb195d5518cc14d3ca66fdc72d5463ed7b5
Tejas Kawale <tejaskawale5@gmail.com>
2023-12-27 23:15:38 +0530

    link added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode!=='light'?props.mode:'light'} bg-${props.mode!=='light'?props.mode:'light'}`}>
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">{props.title}</a>  
+      <a className="navbar-brand" href="https://www.youtube.com/watch?v=3s08AlYB7yg&ab_channel=HarkiratSingh">{props.title}</a>  
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
